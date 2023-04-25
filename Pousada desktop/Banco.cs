4be4b5778119096f1aadac535e59707ddce39e5d
PEDRO HENRIQUE TORRES GONCALVES <pedro.htgoncalves1@senacsp.edu.br>
2023-04-25 17:07:41 -0300

    Adicionar arquivos de projeto.

---
+++ Pousada desktop/Banco.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pousada_desktop
+{
+    internal class Banco
+    {
+    }
+}
