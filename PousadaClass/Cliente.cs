7807199f9f89c85286221870c67af14563b821d6
PEDRO HENRIQUE TORRES GONCALVES <pedro.htgoncalves1@senacsp.edu.br>
2023-05-03 17:22:40 -0300

    Obter por id com falha e inserir Cliente

--- PousadaClass/Cliente.cs
+++ PousadaClass/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,7 +30,8 @@ namespace PousadaClass
         public string Email { get => email; set => email = value; }
         public string Recupera { get => recupera; set => recupera = value; }
         public DateTime Arquivar { get => arquivar; set => arquivar = value; }
-
+        public List<ClienteTelefone> Telefones { get; set; }
+        public List<ClienteEndereco> Enderecos { get; set; }
 
         // --------- Metodos Construtores -----------------
         public Cliente (){ }
@@ -63,8 +65,65 @@ namespace PousadaClass
             Arquivar = arquivar;
         }
 
+        // --------------------------- INSERINDO --------------------------------
+        /// <summary>
+        ///  Inserindo o usuario no Banco  de dados 
+        /// </summary>
+        /// 
 
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert clientes (nome,cpf, rg, senha, email)" +
+              "values (@nome, @cpf, @rg,@senha,@email) ";
+            cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = Nome;
+            cmd.Parameters.Add("@cpf", MySqlDbType.VarChar).Value = Cpf;
+            cmd.Parameters.Add("@rg", MySqlDbType.VarChar).Value = Rg;
+            cmd.Parameters.Add("@senha", MySqlDbType.VarChar).Value = Senha;
+            cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = Email;
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "select @@identity";
+            // recupera o id na Propriedade
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
+            foreach (var telefone in Telefones)
+            {
+                telefone.Inserir(Id);
+            }
+            foreach (var endereco in Enderecos)
+            {
+                endereco.Inserir(Id);
+            }
+            // fecha  a conexão
+            Banco.Fechar(cmd);
 
+        }
+        // ------------------------------- OBTENDO POR ID ------------------------ 
 
+        /// <summary>
+        ///  Obtendo o id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static Cliente ObterPorId(int id)
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from clientes where id = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            var dr = cmd.ExecuteReader();
+            Cliente cliente = null;
+            while (dr.Read())
+            {
+                cliente = new Cliente(
+                dr.GetInt32(0),
+                dr.GetString(1),
+                dr.GetString(2),
+                dr.GetString(3),
+                dr.GetString(4),
+                dr.GetString(5)
+                    );
+                }
+            }
+        }
     }
-}
